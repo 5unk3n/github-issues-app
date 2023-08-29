5850da417d783490b604dd8721241b45dd8436a0
Seongeun Lee <tjsrms1227@gmail.com>
2023-08-30 00:40:14 +0900

    Feat: App에 공통 레이아웃 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import Layout from './components/Layout/Layout';
 import Router from './routes/Router';
 
 function App() {
-  return <Router />;
+  return (
+    <Layout>
+      <Router />
+    </Layout>
+  );
 }
 
 export default App;
