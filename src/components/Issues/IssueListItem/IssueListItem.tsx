10f44e61373b397751bae1b8e85feab26ed989de
Seongeun Lee <tjsrms1227@gmail.com>
2023-08-30 15:36:22 +0900

    Feat: 이슈 작성일 형식 변경
    - YYYY년 MM월 DD일 형식으로 변경

--- src/components/Issues/IssueListItem/IssueListItem.tsx
+++ src/components/Issues/IssueListItem/IssueListItem.tsx
@@ -7,6 +7,12 @@ interface IssueListItemProps {
 }
 
 const IssueListItem = ({ issue }: IssueListItemProps) => {
+  const formattedDate = new Date(issue.created_at).toLocaleDateString('ko-KR', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  });
+
   return (
     <S.IssueItemWrapper>
       <div>
@@ -16,7 +22,7 @@ const IssueListItem = ({ issue }: IssueListItemProps) => {
         </S.TitleWrapper>
         <S.UserWrapper>
           <span>{`작성자: ${issue.user.login}`}</span>
-          <span>{`, 작성일: ${issue.created_at}`}</span>
+          <span>{`, 작성일: ${formattedDate}`}</span>
         </S.UserWrapper>
       </div>
       <S.Comment>{`코멘트: ${issue.comments}`}</S.Comment>
